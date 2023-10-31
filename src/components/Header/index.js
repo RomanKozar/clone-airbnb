1001c349388dea8b0b62911ad3a498947015f8e7
RomanKozar <kozarrma2004@gmail.com>
2023-11-01 00:26:37 +0200

    action

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,3 +1,20 @@
-import Header from "./Header";
+import React from "react";
+
+import logo from "../../assets/img/logotip.png";
+import "./styles.css";
+
+function Header() {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" className="navbar-logo" />
+      <div className="search-bar">
+        <div className="search-bar-text">Будь-куди</div>
+        <div className="search-bar-text">Будь-який тиждень</div>
+        <div className="search-bar-text2">Додайте гостей</div>
+      </div>
+      <div className="profile-container"></div>
+    </div>
+  );
+}
 
 export default Header;
