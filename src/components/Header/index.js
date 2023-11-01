ce4507f7de662de02c7576224db69590660ae381
RomanKozar <kozarrma2004@gmail.com>
2023-11-01 14:52:32 +0200

    action

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-import logo from "../../assets/img/logotip.png";
 import "./styles.css";
+import logo from "../../assets/img/logotip.png";
+import SearchRoundedIcon from "../../assets/img/search.svg";
+import LanguageIcon from "../../assets/img/Langues.svg";
 
 function Header() {
   return (
@@ -11,8 +13,26 @@ function Header() {
         <div className="search-bar-text">Будь-куди</div>
         <div className="search-bar-text">Будь-який тиждень</div>
         <div className="search-bar-text2">Додайте гостей</div>
+        <div className="search-icon-div">
+          <img src={SearchRoundedIcon} alt="icon" className="search-icon" />
+        </div>
       </div>
-      <div className="profile-container"></div>
+      <div className="profile-container">
+        <div className="airbnb-your-home">
+          Запропонувати помешкання на Airbnb
+        </div>
+        <div className="global-div">
+          <img
+            src={LanguageIcon}
+            alt="laeng"
+            className="language-icon"
+            sx={{ fontSize: "1.5rem" }}
+          />
+        </div>
+        <div className="profile-div">
+          {/* <img src={LanguageIcon} alt="laeng" /> */}
+        </div>
+      </div>
     </div>
   );
 }
