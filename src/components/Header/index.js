59fb71f38ec7961ae45444bfb182214674b18fac
RomanKozar <kozarrma2004@gmail.com>
2023-11-04 17:34:56 +0200

    route

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,49 +7,40 @@ import LanguageIcon from "../../assets/img/Langues.svg";
 import BasicMenu from "./ProfileMenu";
 import SimpleBottomNavigation from "./BottomNav";
 import MobileSearchBar from "../MobileSearchBar";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-// import PremisesAirbnb from "../Pages/PremisesAirbnb";
 
 function Header() {
   return (
-    <>
-      <div className="navbar">
-        <img src={logo} alt="logo" className="navbar-logo" />
-        <div className="search-bar">
-          <div className="search-bar-text">Будь-куди</div>
-          <div className="search-bar-text">Будь-який тиждень</div>
-          <div className="search-bar-text2">Додайте гостей</div>
-          <div className="search-icon-div">
-            <img src={SearchRoundedIcon} alt="icon" className="search-icon" />
-          </div>
+    <div className="navbar">
+      <img src={logo} alt="logo" className="navbar-logo" />
+      <div className="search-bar">
+        <div className="search-bar-text">Будь-куди</div>
+        <div className="search-bar-text">Будь-який тиждень</div>
+        <div className="search-bar-text2">Додайте гостей</div>
+        <div className="search-icon-div">
+          <img src={SearchRoundedIcon} alt="icon" className="search-icon" />
         </div>
-        <div className="profile-container">
-          <div className="airbnb-your-home">
-            {/* <Link to="/premises">Запропонувати помешкання на Airbnb</Link> */}
-            Запропонувати помешкання на Airbnb
-          </div>
-          <div className="global-div">
-            <img
-              src={LanguageIcon}
-              alt="laeng"
-              className="language-icon"
-              sx={{ fontSize: "1.3rem" }}
-            />
-          </div>
-          <div className="profile-div">
-            <BasicMenu />
-          </div>
-        </div>
-        <MobileSearchBar />
-        <SimpleBottomNavigation />
       </div>
-      {/* <Router>
-        <Switch>
-          <Route exact path="/premises" component={PremisesAirbnb} />
-        </Switch>
-      </Router> */}
-    </>
+      <div className="profile-container">
+        <div className="airbnb-your-home">
+          {/* <Link to="/premises">Запропонувати помешкання на Airbnb</Link> */}
+          Запропонувати помешкання на Airbnb
+        </div>
+        <div className="global-div">
+          <img
+            src={LanguageIcon}
+            alt="laeng"
+            className="language-icon"
+            sx={{ fontSize: "1.3rem" }}
+          />
+        </div>
+        <div className="profile-div">
+          <BasicMenu />
+        </div>
+      </div>
+      <MobileSearchBar />
+      <SimpleBottomNavigation />
+    </div>
   );
 }
 
